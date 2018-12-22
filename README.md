d84ae00c7741fa49b522e5357dbfc769d213ba17
Boris Raicheff <b@raicheff.com>
2018-12-22 00:25:35 +0000

    Hello, world!

---
+++ README.md
@@ -0,0 +1,2 @@
+Flask-DebugToolbar-More
+=======================
---
+++ flask_debugtoolbar_more/environ.py
@@ -0,0 +1,49 @@
+#
+# Flask-DebugToolbar-More
+#
+# Copyright (C) 2018 Airpbx Ltd
+# All rights reserved
+#
+
+
+import os
+
+from flask_debugtoolbar.panels import DebugPanel
+from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
+
+
+_ = lambda x: x
+
+
+class EnvironDebugPanel(DebugPanel):
+    """"""
+
+    name = 'Environ'
+
+    has_content = True
+
+    def __init__(self, jinja_env, **kwargs):
+        super().__init__(jinja_env, **kwargs)
+        jinja_env.loader = ChoiceLoader([
+            jinja_env.loader,
+            PrefixLoader({
+                'flask-debugtoolbar-more': PackageLoader(__name__, 'templates')
+            })
+        ])
+
+    def nav_title(self):
+        return _('Environment')
+
+    def title(self):
+        return _('Environment')
+
+    def url(self):
+        return ''
+
+    def content(self):
+        context = self.context.copy()
+        context.update({'environ': os.environ})
+        return self.render('flask-debugtoolbar-more/environ.html', context)
+
+
+# EOF
